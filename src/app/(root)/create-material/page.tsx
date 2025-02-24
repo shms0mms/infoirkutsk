3d14d9d95253a6a0f26ff5ee6dca75bd40a162f0
shms0mms <shmsmms01@gmail.com>
2025-02-24 14:35:56 +0800

    merged: all changes

--- src/app/(root)/create-material/page.tsx
+++ src/app/(root)/create-material/page.tsx
@@ -4,11 +4,7 @@ import { CreateMaterialForm } from "@/app/@modal/(.)create-material/create-mater
 export default function CreateMaterial() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
-<<<<<<< HEAD
-      {<CreateMaterialForm />}
-=======
       <CreateMaterialForm />
->>>>>>> 9488f51772810ded4c45f39f8907e5de4d9baec6
     </Suspense>
   )
 }
